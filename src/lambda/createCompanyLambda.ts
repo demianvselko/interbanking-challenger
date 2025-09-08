e5ec3cc2a9674113a07f077435b28e388a886840
demianvselko <vselkod@gmail.com>
2025-09-08 09:57:46 -0300

    add info to readme

--- src/lambda/createCompanyLambda.ts
+++ src/lambda/createCompanyLambda.ts
@@ -1,46 +1,10 @@
-
 import { CreateCompanyHttpRequest } from '@interface/http/dto/createCompany.http.dto';
 import { CreateCompanyUseCase } from '@application/company/useCases/createCompany.useCase';
 import { CompanyRepositoryImpl } from '@infrastructure/repositories/json/company.repository.impl';
 import { Result } from '@context/shared/result';
 
 export async function createCompanyHandler(
-  event: CreateCompanyHttpRequest,
-): Promise<{ statusCode: number; body: any }> {
-  const companyRepo = new CompanyRepositoryImpl();
-  const useCase = new CreateCompanyUseCase(companyRepo);
-
-  let statusCode = 201;
-  let body;
-
-  try {
-    const result: Result<any> = await useCase.execute({
-      cuit: event.cuit,
-      name: event.name,
-      type: event.type,
-      accounts: event.accounts ?? [],
-    });
-
-    if (!result.ok) {
-      statusCode = 400;
-      body = { error: result.error };
-    } else {
-      body = result.value.toPrimitives();
-    }
-  } catch (err: unknown) {
-    statusCode = 500;
-    body = { error: err instanceof Error ? err.message : 'Unknown error' };
-  }
-
-  return { statusCode, body };
-=======
-import { CreateCompanyHttpRequest } from 'interface/http/dto/createCompany.http.dto';
-import { CreateCompanyUseCase } from 'application/company/useCases/createCompany.useCase';
-import { CompanyRepositoryImpl } from 'infrastructure/repositories/json/company.repository.impl';
-import { Result } from 'context/shraed/result';
-
-export async function createCompanyHandler(
-    event: CreateCompanyHttpRequest
+    event: CreateCompanyHttpRequest,
 ): Promise<{ statusCode: number; body: any }> {
     const companyRepo = new CompanyRepositoryImpl();
     const useCase = new CreateCompanyUseCase(companyRepo);
@@ -68,5 +32,4 @@ export async function createCompanyHandler(
     }
 
     return { statusCode, body };
-
 }
